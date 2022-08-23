d39158535cf31b7914d1fa698daf2bce519962cd
Peter Loft <peterloft3@gmail.com>
2022-08-23 14:57:26 -0500

    added decorative RestController and RequestMapping to '/employee'

--- employeemanager/src/main/java/Controller/EmployeeController.java
+++ employeemanager/src/main/java/Controller/EmployeeController.java
@@ -1,4 +1,10 @@
 package Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+
 }
