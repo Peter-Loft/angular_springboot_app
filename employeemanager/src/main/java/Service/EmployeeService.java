594248e110caf217ba224150771e2a37569b7df6
Peter Loft <peterloft3@gmail.com>
2022-08-23 13:24:04 -0500

    Created the Employee model, Repository, and Service with basic CRUD, findById, and findAllEmployees.

---
+++ employeemanager/src/main/java/Service/EmployeeService.java
@@ -0,0 +1,45 @@
+package Service;
+
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repo.EmployeeRepo;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findById(id).orElseThrow(
+                () -> new IllegalStateException(
+                        "User " + id + " was not found"
+                )
+        );
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
---
+++ employeemanager/src/main/java/repo/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package repo;
+
+import model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+}
